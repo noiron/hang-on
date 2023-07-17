6479bb788916e131ce4aba682236c4d9424c4815
wu kai <wukai2048@gmail.com>
2023-07-17 09:50:38 +0800

    add/remove page from popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,28 +1,79 @@
-import { useState } from "react"
+import { useStorage } from "@plasmohq/storage/hook"
+import { useEffect, useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [blockedSites, setBlockedSites] = useStorage<string[]>(
+    "blockedSites",
+    []
+  )
+  const [isBlocked, setIsBlocked] = useState(false)
+  const [matchedSite, setMatchedSite] = useState("")
+  const [currentUrl, setCurrentUrl] = useState<URL>(null)
+
+  useEffect(() => {
+    async function check() {
+      if (currentUrl) return
+
+      // 需要添加 permissions activeTab 才能获取 url
+      const url = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      })
+      if (!url.length) return
+
+      if (url[0]) setCurrentUrl(new URL(url[0].url))
+    }
+
+    check()
+  })
+
+  useEffect(() => {
+    let blocked = false
+    for (let i = 0; i < blockedSites.length; i++) {
+      const site = blockedSites[i]
+      if (currentUrl.origin.indexOf(site) > -1) {
+        setMatchedSite(site)
+        blocked = true
+        break
+      }
+    }
+    setIsBlocked(blocked)
+  }, [currentUrl, blockedSites])
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
+        width: 200,
+        padding: 8
       }}>
-        2
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      {isBlocked ? (
+        <div>
+          <h2>This site is blocked</h2>
+          <p style={{ fontSize: 18 }}>{matchedSite}</p>
+          <button
+            onClick={() => {
+              const index = blockedSites.indexOf(matchedSite)
+              const newBlockedSites = [...blockedSites]
+              newBlockedSites.splice(index, 1)
+              setBlockedSites(newBlockedSites)
+            }}>
+            Remove
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2>Do you want to block this site ?</h2>
+          <p style={{ fontSize: 18 }}>{currentUrl?.host}</p>
+          <button
+            onClick={() => {
+              const host = currentUrl.host
+              const newBlockedSites = [...blockedSites, host]
+              setBlockedSites(newBlockedSites)
+            }}>
+            Block
+          </button>
+        </div>
+      )}
     </div>
   )
 }
