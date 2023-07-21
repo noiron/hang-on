b59cbcc89ce4bef5a25de40d44f09b84d004c719
wu kai <wukai2048@gmail.com>
2023-07-21 15:54:14 +0800

    update options style

--- src/options.tsx
+++ src/options.tsx
@@ -1,6 +1,12 @@
 import { useStorage } from "@plasmohq/storage/hook"
 import { useState } from "react"
 import hangingSlothImage from "data-base64:~assets/hanging-sloth.png"
+import "./style.css"
+
+const inputClassNames =
+  "shadow appearance-none border rounded w-16 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+const buttonClassNames =
+  "bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded w-16"
 
 const Options = () => {
   const [waitTime, setWaitTime] = useStorage<number>("waitTime", 10)
@@ -11,69 +17,67 @@ const Options = () => {
   const [site, setSite] = useState("")
 
   return (
-    <div
-      style={{
-        margin: 20
-      }}>
-      <h1>Hang On a Moment</h1>
-      <img
-        src={hangingSlothImage}
-        style={{
-          width: 200,
-          margin: 20
-        }}></img>
-      <p>
-        <h2>Wait time</h2>
-        <input
-          type="number"
-          min={3}
-          value={waitTime}
-          onChange={(e) => {
-            setWaitTime(parseInt(e.target.value))
-          }}
-          style={{
-            width: 50
-          }}
-        />
-        <span>S</span>
-      </p>
+    <div className="m-4 p-4 bg-white">
+      <h1 className="text-2xl font-bold text-center">Hang On a Moment</h1>
+      <img src={hangingSlothImage} className="w-48 my-5 mx-auto" />
 
-      <div>
-        <h2>Blocked Sites:</h2>
-        <ul>
-          {blockedSites.map((site, index) => {
-            return (
-              <li style={{ margin: "10px 0" }}>
-                {site}
-                <button
-                  style={{ marginLeft: 10 }}
-                  onClick={() => {
-                    const newBlockedSites = [...blockedSites]
-                    newBlockedSites.splice(index, 1)
-                    setBlockedSites(newBlockedSites)
-                  }}>
-                  Remove
-                </button>
-              </li>
-            )
-          })}
-        </ul>
-        <p>Enter the page you want to block. Eg. twitter.com</p>
-        <input
-          value={site}
-          style={{ width: 220 }}
-          onChange={(e) => {
-            setSite(e.target.value)
-          }}></input>
-        <button
-          style={{ marginLeft: 10 }}
-          onClick={() => {
-            const newBlockedSites = [...blockedSites, site]
-            setBlockedSites(newBlockedSites)
-            setSite("")
-          }}>
-          Add
-        </button>
+      <div className="w-1/2 m-auto">
+        <p>
+          <h2 className="text-lg inline mr-3">Wait time</h2>
+          <input
+            type="number"
+            min={3}
+            value={waitTime}
+            onChange={(e) => {
+              setWaitTime(parseInt(e.target.value))
+            }}
+            className={inputClassNames + " mr-2"}
+          />
+          <span>Seconds</span>
+        </p>
+
+        <p className="text-lg my-2">
+          Enter the page you want to block. Eg. twitter.com
+        </p>
+        <div className="w-full flex items-center">
+          <input
+            className={inputClassNames + " flex-auto"}
+            value={site}
+            onChange={(e) => {
+              setSite(e.target.value)
+            }}></input>
+          <button
+            className={buttonClassNames + " ml-2"}
+            onClick={() => {
+              const newBlockedSites = [...blockedSites, site]
+              setBlockedSites(newBlockedSites)
+              setSite("")
+            }}>
+            Add
+          </button>
+        </div>
+
+        <div>
+          <h2 className="text-lg my-2">Blocked Sites</h2>
+          <ul className="list-disc list-inside">
+            {blockedSites.map((site, index) => {
+              return (
+                <li className="mb-2 flex justify-between items-center pb-2 border-b">
+                  {site}
+                  <button
+                    className={buttonClassNames}
+                    onClick={() => {
+                      const newBlockedSites = [...blockedSites]
+                      newBlockedSites.splice(index, 1)
+                      setBlockedSites(newBlockedSites)
+                    }}>
+                    Remove
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   )
