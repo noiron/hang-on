c0ebc36ca730d154aff7ef96c38bfc7e85a42f9d
wu kai <wukai2048@gmail.com>
2023-07-22 10:34:05 +0800

    button & input component

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,12 +1,9 @@
 import { useStorage } from "@plasmohq/storage/hook"
 import { useState } from "react"
 import hangingSlothImage from "data-base64:~assets/hanging-sloth.png"
-import "./style.css"
-
-const inputClassNames =
-  "shadow appearance-none border rounded w-16 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-const buttonClassNames =
-  "bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded w-16"
+import "../style.css"
+import Input from "./input"
+import Button from "./button"
 
 const Options = () => {
   const [waitTime, setWaitTime] = useStorage<number>("waitTime", 10)
@@ -24,14 +21,14 @@ const Options = () => {
       <div className="w-1/2 m-auto">
         <p>
           <h2 className="text-lg inline mr-3">Wait time</h2>
-          <input
+          <Input
             type="number"
             min={3}
             value={waitTime}
-            onChange={(e) => {
-              setWaitTime(parseInt(e.target.value))
+            className="mr-2"
+            onChange={(value: string) => {
+              setWaitTime(parseInt(value))
             }}
-            className={inputClassNames + " mr-2"}
           />
           <span>Seconds</span>
         </p>
@@ -39,22 +36,22 @@ const Options = () => {
         <p className="text-lg my-2">
           Enter the page you want to block. Eg. twitter.com
         </p>
-        <div className="w-full flex items-center">
-          <input
-            className={inputClassNames + " flex-auto"}
+        <div className="w-full flex items-center pr-2">
+          <Input
             value={site}
-            onChange={(e) => {
-              setSite(e.target.value)
-            }}></input>
-          <button
-            className={buttonClassNames + " ml-2"}
-            onClick={() => {
+            onChange={(value: string) => setSite(value)}
+            className="flex-auto"
+          />
+
+          <Button
+            handleClick={() => {
               const newBlockedSites = [...blockedSites, site]
               setBlockedSites(newBlockedSites)
               setSite("")
-            }}>
+            }}
+            className="ml-2">
             Add
-          </button>
+          </Button>
         </div>
 
         <div>
@@ -62,17 +59,18 @@ const Options = () => {
           <ul className="list-disc list-inside">
             {blockedSites.map((site, index) => {
               return (
-                <li className="mb-2 flex justify-between items-center pb-2 border-b">
+                <li
+                  className="flex justify-between items-center py-2 px-2
+                hover:bg-slate-100 rounded border-b border-slate-200">
                   {site}
-                  <button
-                    className={buttonClassNames}
-                    onClick={() => {
+                  <Button
+                    handleClick={() => {
                       const newBlockedSites = [...blockedSites]
                       newBlockedSites.splice(index, 1)
                       setBlockedSites(newBlockedSites)
                     }}>
                     Remove
-                  </button>
+                  </Button>
                 </li>
               )
             })}
